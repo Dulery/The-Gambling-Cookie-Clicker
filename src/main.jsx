fb4add09a94421701c09b2bd7db12851b87295d2
Dulery <lauric.dulery@epitech.eu>
2026-05-20 16:18:35 +0200

    feat: vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AuthProvider } from 'react-oidc-context'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
 
@@ -20,5 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthProvider {...oidcConfig}>
       <App />
     </AuthProvider>
+    <Analytics />
   </React.StrictMode>
 )
